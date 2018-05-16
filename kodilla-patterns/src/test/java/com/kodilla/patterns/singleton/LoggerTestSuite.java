9abc5cd76586bc3a3d2846acb61202c4ba333bd0
Aspink <aspink1@wp.pl>
2018-05-16 13:10:40 +0200

    Zadanie 11.1 singleton

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -1,21 +1,18 @@
 package com.kodilla.patterns.singleton;
 
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class LoggerTestSuite {
-    private static Logger logger;
 
     @Test
     public void testGetLastLog() {
 
         //Given
-        logger = new Logger();
-        logger.log("The logger is started");
+        Logger.getInstance().log("The logger is started");
 
         //When
-        String log = logger.getLastLog();
+        String log = Logger.getInstance().getLastLog();
         System.out.println("Last log: " + log);
 
         //Then
